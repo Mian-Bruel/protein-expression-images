bbf8019335f9c7f8c4db3301ee01114b5ac75e82
Samuel Janas <sneakyinsect@gmail.com>
2023-10-22 12:57:41 +0200

    extract pathology patient info

--- xml_utils/xml_parser.py
+++ xml_utils/xml_parser.py
@@ -1,5 +1,7 @@
 import xml.etree.ElementTree as ET
 
+import pandas as pd
+
 
 def load_xml(filename):
     tree = ET.parse(filename)
@@ -15,14 +17,70 @@ def get_synonyms(entry):
     return [syn.text for syn in entry.findall("synonym")]
 
 
+def get_patient_info(root, names):
+    # Create an empty list to store patient info for each image
+    patient_images_info = []
+
+    # Iterate through each tissueCell element in the XML
+    for tissue_cell in root.findall(".//tissueExpression"):
+        summary = tissue_cell.find("summary")
+
+        # if type is not 'pathology', skip this tissueCell
+        if summary.attrib["type"] != "pathology":
+            continue
+        # Iterate through each patient element inside the current tissueCell
+        for patient in tissue_cell.findall(".//patient"):
+            # Extract basic patient info
+            patient_id = int(patient.find("patientId").text)
+            sex = patient.find("sex").text
+            age = int(patient.find("age").text)
+
+            staining_element = patient.find("level[@type='staining']")
+            staining = staining_element.text if staining_element is not None else None
+
+            intensity = patient.find("level[@type='intensity']")
+            intensity = intensity.text if intensity is not None else None
+
+            quantity = patient.find("quantity")
+            quantity = quantity.text if quantity is not None else None
+
+            location = patient.find("location")
+            location = location.text if location is not None else None
+
+            # Extract SNOMED descriptions and images
+            for sample in patient.findall(".//sample"):
+                snomed_parameters = sample.find("snomedParameters")
+                tissue_descriptions = [snomed.attrib["tissueDescription"] for snomed in snomed_parameters.findall("snomed")]
+
+                assay_image = sample.find("assayImage")
+                for image in assay_image.findall("image"):
+                    image_url = image.find("imageUrl").text
+                    patient_images_info.append(
+                        {
+                            "gene_names": names,  # This is a list of gene synonyms
+                            "patientId": patient_id,
+                            "sex": sex,
+                            "age": age,
+                            "staining": staining,
+                            "intensity": intensity,
+                            "quantity": quantity,
+                            "location": location,
+                            "tissueDescriptions": tissue_descriptions,
+                            "imageUrl": image_url,
+                        }
+                    )
+    return patient_images_info
+
+
 def process_xml(xml):
-    info = {}
     synonyms = []
     for entry in xml.findall("entry"):
         name = get_name(entry)
         synonyms = get_synonyms(entry)
 
-    info["names"] = [name] + synonyms
+    names = [name] + synonyms
+    info = get_patient_info(xml, names)
+
     return info
 
 
@@ -30,4 +88,5 @@ if __name__ == "__main__":
     filename = "xml_files/SLC2A3_latest.xml"
     xml = load_xml(filename)
     info = process_xml(xml)
-    print(info)
+    info_df = pd.DataFrame(info)
+    print(info_df.head())
