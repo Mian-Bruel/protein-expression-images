2a3018f47e1c5a602392318fff383b587d4e8ad0
Michał Kamiński <101565749+michalkamin@users.noreply.github.com>
2023-11-07 18:36:13 +0100

    interactions from html (#3)
    * interactions from html
    
    * Changed dataframe to contain gene name column
    
    Co-authored-by: Samuel Janas <35274379+SamuelJanas@users.noreply.github.com>
    
    * fix - return empty dataframe if gene has no interactions
    
    * clean-up
    
    * clean-up v2
    
    ---------
    
    Co-authored-by: Samuel Janas <35274379+SamuelJanas@users.noreply.github.com>

---
+++ xml_utils/interactions.py
@@ -0,0 +1,42 @@
+import bs4
+import requests
+import pandas as pd
+
+
+def get_interactions_from_html(gene: str, url: str) -> pd.DataFrame:
+    response = requests.get(url)
+
+    parsed = bs4.BeautifulSoup(response.text, "html.parser")
+
+    # dataframe of interactions
+    table = parsed.find("table", class_="sortable")
+
+    if table is None:
+        return pd.DataFrame(columns=["Interaction", "Interaction type", "Confidence", "MI score", "# Interactions"])
+
+    headers = [header.text for header in table.find("thead").find_all("th")]
+
+    rows = table.find("tbody").find_all("tr")
+
+    # Initialize an empty list to store the data
+    data = []
+
+    # Iterate through the rows and extract data
+    for row in rows:
+        cells = row.find_all(["td", "th"])  # Both data cells and header cells
+        row_data = [cell.get_text(strip=True) for cell in cells]
+        data.append(row_data)
+
+    gene_list = [gene] * len(rows)
+    # Create a Pandas DataFrame
+    df = pd.DataFrame(data, columns=headers)
+    df["gene"] = gene_list
+
+    return df
+
+
+if __name__ == "__main__":
+    gene = "SLC2A3"
+    ensID = "ENSG00000059804"
+    url = f"https://www.proteinatlas.org/{ensID}-{gene}/interaction"
+    print(get_interactions_from_html(gene, url))
