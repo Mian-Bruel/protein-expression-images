d2efcf36f7aa7cca81e754c22378a0d1bed68136
Samuel Janas <35274379+SamuelJanas@users.noreply.github.com>
2023-12-04 21:12:11 +0100

    Move backend (#7)
    * Move dashboard functionality
    
    * docstrings for easier navigation
    
    * Added support for web rest api
    
    * basic pagination
    
    * supa secret serva
    
    * all your prints are belong to us
    
    * what doesn't work, shouldn't be present
    
    * wololo
    
    * rest api downloads
    
    ---------
    
    Co-authored-by: 2002jan <2002jan@gmail.com>

---
+++ dashboard/app.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import streamlit as st
+from dotenv import load_dotenv
+from constants import PAGE_CONFIG
+from sidebar import render_sidebar
+from data_processing import process_data
+from download_handlers import handle_downloads
+
+from xml_utils.xml_loader import download_lookup_df
+
+# Set page configuration
+st.set_page_config(**PAGE_CONFIG)
+
+PAGE_SIZE = 100
+
+
+def main(lookup_df):
+    # Initialize session state to store the filtered dataframe and gene selections
+    if (
+        "filtered_df" not in st.session_state
+        or "interactions_df" not in st.session_state
+        or "total_number" not in st.session_state
+    ):
+        st.session_state["filtered_df"] = pd.DataFrame()
+        st.session_state["interactions_df"] = pd.DataFrame()
+        st.session_state["total_number"] = 0
+
+    # FIXME: streamlit refreshes this every two-ish seconds
+    # It's not a huge deal, but it might be beneficial to fix.
+
+    # Render the sidebar and get the values from it
+    _, filters, selected_genes = render_sidebar(lookup_df)
+
+    max_pages = (st.session_state["total_number"] // PAGE_SIZE + 1) if st.session_state["total_number"] > 0 else 1
+    # page button:
+    cols = st.columns([2, 1, 2])
+    page = cols[1].number_input("Page", min_value=1, max_value=max_pages, value=1, step=1)
+
+    st.session_state["filtered_df"], st.session_state["interactions_df"], st.session_state["total_number"] = process_data(
+        filters, selected_genes, lookup_df, page
+    )
+
+    dataframe_columns = st.columns([1, 5, 1])
+    # Display the data
+    dataframe_columns[1].markdown(
+        f"Displaying **{len(st.session_state['filtered_df'])}** out of **{st.session_state['total_number']}** results"
+    )
+    dataframe_columns[1].dataframe(st.session_state["filtered_df"])
+
+    # TODO: st.dataframe(st.session_state["interactions_df"])
+
+    # Display download buttons / handle their clicks
+    handle_downloads(filters, selected_genes)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    lookup_df = download_lookup_df()
+    main(lookup_df=lookup_df)
---
+++ dashboard/data_processing.py
@@ -0,0 +1,80 @@
+import pandas as pd
+from utils import send_request
+
+from xml_utils.xml_loader import get_gene_xml_url
+from xml_utils.interactions import get_interactions_from_html
+
+
+def process_data(filters, selected_genes, lookup_df, page):
+    """
+    Process the data based on the filters and selected genes
+
+    This function is called when the 'Apply Changes' button is clicked.
+
+
+    Args:
+        filters (dict): A dictionary of the values of the filters.
+        selected_genes (list): A list of the selected genes.
+        lookup_df (pd.DataFrame): The lookup dataframe. With the gene names and their corresponding XML URLs.
+
+    Returns:
+        filtered_df (pd.DataFrame): The filtered dataframe.
+        interactions (pd.DataFrame): The interactions dataframe.
+
+    TODO: Communicate with the server with the selected genes and filters to get the filtered dataframe.
+        We'll be using requests library to do this.
+    For now the interactions dataframe is left as is. We might add it to PHP later.
+
+    """
+    # Perform the data processing only when the 'Apply Changes' button is clicked
+
+    interactions = pd.DataFrame(columns=["Interaction", "Interaction type", "Confidence", "MI score", "# Interactions"])
+    for gene in selected_genes:
+        break
+        # TODO: interactions from rest api
+        xml_url, interaction_url = get_gene_xml_url(gene, lookup_df)
+        interaction_df = get_interactions_from_html(gene=gene, url=interaction_url)
+        interactions = interactions.merge(interaction_df, how="outer")
+
+    response = send_request(filters, "list", selected_genes=selected_genes, page=page)
+
+    # If the response is not 200, return an empty dataframe
+    if response.status_code != 200:
+        return pd.DataFrame(), interactions, 0
+
+    response_data = response.json()
+
+    total_number = response_data["totalItems"]
+    data = response_data["data"]
+    processed_data = []
+
+    for entry in data:
+        entry_data = {
+            "staining": entry["staining"],
+            "intensity": entry["intensity"],  # FIXME: Grep failed
+            "quantity": entry["quantity"],
+            "location": entry["location"],
+            "patientId": entry["patient"]["id"],
+            "patientAge": entry["patient"]["age"],
+            "patientSex": entry["patient"]["sex"],
+            "geneName": entry["gene"]["name"],
+        }
+
+        images = []
+        tissue_description = ""
+
+        for sample in entry["samples"]:
+            images.append(sample["img"])
+            description = sample["tissueDescription"]
+
+            if len(description) > len(tissue_description):
+                tissue_description = description
+
+        entry_data["tissue_description"] = tissue_description
+        entry_data["images"] = ", ".join(images)
+
+        processed_data.append(entry_data)
+
+    filtered_df = pd.DataFrame(processed_data)
+
+    return filtered_df, interactions, total_number
